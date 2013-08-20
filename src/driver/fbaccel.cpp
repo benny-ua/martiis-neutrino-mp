d4c2a9f0271e3706892f87ff071b200dd20f202b
martii <m4rtii@gmx.de>
2013-08-20 18:40:37 +0200

    fbaccel: fix blitBPA2FB prototype

--- src/driver/fbaccel.cpp
+++ src/driver/fbaccel.cpp
@@ -1024,7 +1024,7 @@ void CFbAccel::mark(int, int, int, int)
 #endif
 
 #if HAVE_SPARK_HARDWARE
-void CFbAccel::blitBPA2FB(unsigned char *mem, SURF_FMT fmt, int w, int h, int x, int y, int pan_x, int pan_y, int fb_x, int fb_y, int fb_w, int fb_h, int transp)
+void CFbAccel::blitBPA2FB(unsigned char *mem, SURF_FMT fmt, int w, int h, int x, int y, int pan_x, int pan_y, int fb_x, int fb_y, int fb_w, int fb_h, bool transp)
 {
 	if (w < 1 || h < 1)
 		return;
