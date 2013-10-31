9e2a6bad37479750f7ba481cfaba6a93be7bc8a1
martii <m4rtii@gmx.de>
2013-10-31 21:29:00 +0100

    driver/audiodec: don't encode utf8 to utf8

--- src/system/settings.h
+++ src/system/settings.h
@@ -39,6 +39,7 @@
 #include <configfile.h>
 #include <zapit/client/zapitclient.h>
 #include <zapit/client/zapittools.h>
+#include <eitd/edvbstring.h> // UTF8
 
 #include <hardware_caps.h>
 
@@ -676,9 +677,11 @@ struct SNeutrinoSettings
 	int backlight_tv;
 	int backlight_standby;
 	int backlight_deepstandby;
-#define FILESYSTEM_ENCODING_TO_UTF8(a) (g_settings.filesystem_is_utf8 ? (a) : ZapitTools::Latin1_to_UTF8(a).c_str())
+//#define FILESYSTEM_ENCODING_TO_UTF8(a) (g_settings.filesystem_is_utf8 ? (a) : ZapitTools::Latin1_to_UTF8(a).c_str())
+#define FILESYSTEM_ENCODING_TO_UTF8(a) (isUTF8(a) ? (a) : ZapitTools::Latin1_to_UTF8(a).c_str())
 #define UTF8_TO_FILESYSTEM_ENCODING(a) (g_settings.filesystem_is_utf8 ? (a) : ZapitTools::UTF8_to_Latin1(a).c_str())
-#define FILESYSTEM_ENCODING_TO_UTF8_STRING(a) (g_settings.filesystem_is_utf8 ? (a) : ZapitTools::Latin1_to_UTF8(a))
+//#define FILESYSTEM_ENCODING_TO_UTF8_STRING(a) (g_settings.filesystem_is_utf8 ? (a) : ZapitTools::Latin1_to_UTF8(a))
+#define FILESYSTEM_ENCODING_TO_UTF8_STRING(a) (isUTF8(a) ? (a) : ZapitTools::Latin1_to_UTF8(a))
 
 	// pictureviewer
 	std::string picviewer_slide_time; //FIXME
