75343b8fcaaa9361bd8cb1135f2f21ad733194bb
martii <m4rtii@gmx.de>
2013-12-22 13:06:23 +0100

    gui/keybind_setup: add units to repeat blocker chooser

--- src/gui/keybind_setup.cpp
+++ src/gui/keybind_setup.cpp
@@ -272,14 +272,18 @@ int CKeybindSetup::showKeySetup()
 	}
 #endif
 
+	std::string ms_number_format("%d ");
+	ms_number_format += g_Locale->getText(LOCALE_UNIT_SHORT_MILLISECOND);
 	CMenuOptionNumberChooser *cc;
 	cc = new CMenuOptionNumberChooser(LOCALE_KEYBINDINGMENU_REPEATBLOCK,
 		&g_settings.repeat_blocker, true, 0, 999);
+	cc->setNumberFormat(ms_number_format);
 	cc->setHint("", LOCALE_MENU_HINT_KEY_REPEATBLOCK);
 	keySettings->addItem(cc);
 
 	cc = new CMenuOptionNumberChooser(LOCALE_KEYBINDINGMENU_REPEATBLOCKGENERIC,
 		&g_settings.repeat_genericblocker, true, 0, 999);
+	cc->setNumberFormat(ms_number_format);
 	cc->setHint("", LOCALE_MENU_HINT_KEY_REPEATBLOCKGENERIC);
 	keySettings->addItem(cc);
 
