93c7ffbd4801f8c576b14c78f3e959584fefc8e8
martii <m4rtii@gmx.de>
2013-12-26 19:22:35 +0100

    gui/record_setup: don't forget to save recording safety timings

--- src/gui/record_setup.cpp
+++ src/gui/record_setup.cpp
@@ -254,6 +254,7 @@ int CRecordSetup::showRecordSetup()
 	/* activate changes */
         CRecordManager::getInstance()->SetDirectory(g_settings.network_nfs_recordingdir);
         CRecordManager::getInstance()->Config(g_settings.recording_stopsectionsd, g_settings.recording_stream_vtxt_pid, g_settings.recording_stream_pmt_pid, g_settings.recording_stream_subtitle_pids);
+	g_Timerd->setRecordingSafety(g_settings.record_safety_time_before * 60, g_settings.record_safety_time_after * 60);
 
 	return res;
 }
