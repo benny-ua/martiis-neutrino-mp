10b534861e31c07a479ab1fde3a514b1942d0250
martii <m4rtii@gmx.de>
2013-12-30 16:59:16 +0100

    gui/infoclock: fix constructor

--- src/gui/infoclock.cpp
+++ src/gui/infoclock.cpp
@@ -36,7 +36,7 @@
 
 
 
-CInfoClock::CInfoClock():CComponentsFrmClock( 0, 0, 0, 50, "%H:%M:%S", true, CC_SHADOW_ON, COL_LIGHT_GRAY, COL_MENUCONTENT_PLUS_0,COL_MENUCONTENTDARK_PLUS_0)
+CInfoClock::CInfoClock():CComponentsFrmClock( 0, 0, 0, 50, g_settings.infoClockFormat0.c_str(), true, CC_SHADOW_ON, COL_LIGHT_GRAY, COL_MENUCONTENT_PLUS_0,COL_MENUCONTENTDARK_PLUS_0)
 {
 	initVarInfoClock();
 }
