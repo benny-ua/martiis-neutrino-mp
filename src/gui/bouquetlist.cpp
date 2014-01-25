3c74bd197099a95355e7ee54dfcf1c9257440af9
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2014-01-26 00:32:23 +0100

    cherry-pick f625a18324824cc1a5cf4e140e27b6964bedad44 from seife

--- src/gui/bouquetlist.cpp
+++ src/gui/bouquetlist.cpp
@@ -632,8 +632,7 @@ void CBouquetList::paint()
 	frameBuffer->paintBoxRel(x+ width- 15,ypos, 15, sb,  COL_MENUCONTENT_PLUS_1);
 
 	int sbc= ((bsize - 1)/ listmaxshow)+ 1; /* bsize is > 0, so sbc is also > 0 */
-	float sbh= (sb - 4)/ sbc;
 	int sbs= (selected/listmaxshow);
 
-	frameBuffer->paintBoxRel(x+ width- 13, ypos+ 2+ sbs * sbh, 11, sbh, COL_MENUCONTENT_PLUS_3);
+	frameBuffer->paintBoxRel(x+ width- 13, ypos+ 2+ sbs * (sb-4)/sbc, 11, (sb-4)/sbc, COL_MENUCONTENT_PLUS_3);
 }
--- src/gui/pictureviewer.cpp
+++ src/gui/pictureviewer.cpp
@@ -773,15 +773,11 @@ void CPictureViewerGui::paint()
 	frameBuffer->paintBoxRel(x+ width- 15,ypos, 15, sb,  COL_MENUCONTENT_PLUS_1);
 
 	int sbc= ((playlist.size()- 1)/ listmaxshow)+ 1;
-	if (sbc < 1)
-		sbc = 1;
-
-	float sbh= (sb- 4)/ sbc;
 	int sbs= (selected/listmaxshow);
 	if (sbc < 1)
 		sbc = 1;
 
-	frameBuffer->paintBoxRel(x+ width- 13, ypos+ 2+ sbs * sbh, 11, sbh,  COL_MENUCONTENT_PLUS_3);
+	frameBuffer->paintBoxRel(x+ width- 13, ypos+ 2+ sbs * (sb-4)/sbc, 11, (sb-4)/sbc,  COL_MENUCONTENT_PLUS_3);
 
 	paintFoot();
 	paintInfo();
