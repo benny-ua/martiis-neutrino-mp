benny-ua/martiis-neutrino-mp

commit 6eb8fa1c9e3a70a1e60465b838fe954c51e478b6

eitd: Reduce sectionsd memory requirements:
* Various EPG related strings (language- and country keys, audio descriptions) come with a pretty low variance. Cache them, and use an index instead of allocating redundant strings for each and every SIevent.
* Storing content classifications in native format instead of std::string removes quite some memory overhead, too.

This saves about 250 bytes of RAM for every event cached, resulting in a considerably reduced sectionsd memory footprint.

src/eitd/SIevents.hpp
@@ -23,7 +23,7 @@
 #define SIEVENTS_HPP
 
 #include <vector>
-#include <map>
+#include <list>
 #include <set>
 #include <algorithm>
 #include <string>
@@ -31,6 +31,7 @@
 #include <sectionsdclient/sectionsdtypes.h>
 #include <dvbsi++/event_information_section.h>
 #include "edvbstring.h"
+#include "SIlanguage.hpp"
 
 //#define USE_ITEM_DESCRIPTION
 
@@ -123,14 +124,14 @@ public:
 	}
 
 	void dump(void) const {
-		printf("Linakge Type: 0x%02hhx\n", linkageType);
+		printf("Linkage Type: 0x%02hhx\n", linkageType);
 		if (name.length())
 			printf("Name: %s\n", name.c_str());
 		printf("Transport Stream Id: 0x%04hhx\n", transportStreamId);
 		printf("Original Network Id: 0x%04hhx\n", originalNetworkId);
 		printf("Service Id: 0x%04hhx\n", serviceId);
 	}
-	
+
 	int saveXML(FILE *file) const {
 		fprintf(file, "\t\t\t<linkage type=\"%02x\" linkage_descriptor=\"", linkageType);
 		saveStringToXMLfile(file, name.c_str());
@@ -177,35 +178,41 @@ struct saveSIlinkageXML : public std::unary_function<class SIlinkage, void>
 class SIcomponent 
 {
 	public:
-		std::string component;
+		unsigned int component;
 		unsigned char componentType;
 		unsigned char componentTag;
 		unsigned char streamContent;
 
 		SIcomponent(void) {
+			component = 0;
 			streamContent=0;
 			componentType=0;
 			componentTag=0;      
 		}
 		SIcomponent(const struct descr_component_header *comp) {
+			component = 0;
 			streamContent=comp->stream_content;
 			componentType=comp->component_type;
 			componentTag=comp->component_tag;
 			if(comp->descriptor_length>sizeof(struct descr_component_header)-2)
-				component=convertDVBUTF8(((const char *)comp)+sizeof(struct descr_component_header),
-						comp->descriptor_length-(sizeof(struct descr_component_header)-2), 0, 0);
+				setComponent(convertDVBUTF8(((const char *)comp) + sizeof(struct descr_component_header),
+					comp->descriptor_length-(sizeof(struct descr_component_header)-2), 0, 0));
 		}
 
+		void setComponent(const std::string &component_description);
+		const char *getComponentName() const;
+
 		void dump(void) const {
-			if(component.length())
-				printf("Component: %s\n", component.c_str());
+			const char *comp = getComponentName();
+			if(*comp)
+				printf("Component: %s\n", comp);
 			printf("Stream Content: 0x%02hhx\n", streamContent);
 			printf("Component type: 0x%02hhx\n", componentType);
 			printf("Component tag: 0x%02hhx\n", componentTag);
 		}
 		int saveXML(FILE *file) const {
 			fprintf(file, "\t\t\t<component tag=\"%02x\" type=\"%02x\" stream_content=\"%02x\" text=\"", componentTag, componentType, streamContent);
-			saveStringToXMLfile(file,component.c_str());
+			saveStringToXMLfile(file, getComponentName());
 			fprintf(file, "\"/>\n");
 			return 0;
 		}
@@ -247,25 +254,14 @@ struct saveSIcomponentXML : public std::unary_function<class SIcomponent, void>
 class SIparentalRating 
 {
 	public:
-		std::string countryCode;
-		unsigned char rating; // Bei 1-16 -> Minumim Alter = rating +3
+		unsigned int countryCode;
+		unsigned char rating; // Bei 1-16 -> Minimales Alter = rating +3
 
-		SIparentalRating(const std::string &cc, unsigned char rate) {
-			rating=rate;
-			countryCode=cc;
-		}
+		SIparentalRating(const std::string &cc, unsigned char rate);
 
 		// Der Operator zum sortieren
 		bool operator < (const SIparentalRating& c) const {
 			return countryCode < c.countryCode;
-		}
-		void dump(void) const {
-			printf("Rating: %s %hhu (+3)\n", countryCode.c_str(), rating);
-		}
-		int saveXML(FILE *file) const {
-			if(fprintf(file, "\t\t\t<parental_rating country=\"%s\" rating=\"%hhu\"/>\n", countryCode.c_str(), rating)<0)
-				return 1;
-			return 0;
 		}
 		bool operator==(const SIparentalRating& p) const {
 			return (rating == p.rating) &&
@@ -275,6 +271,8 @@ class SIparentalRating
 			return (rating != p.rating) ||
 				(countryCode != p.countryCode);
 		}
+		void dump(void) const;
+		int saveXML(FILE *file) const;
 };
 //typedef std::set <SIparentalRating, std::less<SIparentalRating> > SIparentalRatings;
 typedef std::vector <SIparentalRating> SIparentalRatings;
@@ -344,9 +342,7 @@ struct saveSItimeXML : public std::unary_function<SItime, void>
 class SIevent
 {
 	private:
-		std::map<std::string, std::string> langName;
-		std::map<std::string, std::string> langText;
-		std::map<std::string, std::string> langExtendedText;
+		std::list<SILangData> langData;
 		int running;
 
 		void parseShortEventDescriptor(const uint8_t *buf, unsigned maxlen);
@@ -378,8 +374,112 @@ class SIevent
 		std::string itemDescription; // Aus dem Extended Descriptor
 		std::string item; // Aus dem Extended Descriptor
 #endif
-		std::string contentClassification; // Aus dem Content Descriptor, als String, da mehrere vorkommen koennen
-		std::string userClassification; // Aus dem Content Descriptor, als String, da mehrere vorkommen koennen
+		struct SIeventClassifications
+		{
+			uint8_t *data;
+			unsigned int size;
+
+			SIeventClassifications& operator = (const SIeventClassifications& c)
+			{
+				if (this != &c) {
+					if (data)
+						free(data);
+					if (c.data) {
+						data = (uint8_t *) malloc(c.size);
+						memcpy(data, c.data, c.size);
+					} else
+						data = NULL;
+					size = c.size;
+				}
+				return *this;
+			}
+
+			SIeventClassifications(const SIeventClassifications& c)
+			{
+				if (this != &c) {
+					if (c.data) {
+						data = (uint8_t *) malloc(c.size);
+						memcpy(data, c.data, c.size);
+					} else
+						data = NULL;
+					size = c.size;
+				}
+			}
+
+			bool operator==(const SIeventClassifications& c) const
+			{
+				if (!data && !c.data)
+					return true;
+				if (!(data && c.data))
+					return false;
+				if (size != c.size)
+					return false;
+				return !memcmp(data, c.data, size);
+			}
+
+			bool operator!=(const SIeventClassifications& c) const
+			{
+				return *this == c;
+			}
+
+			SIeventClassifications()
+			{
+				data = NULL;
+				size = 0;
+			}
+
+			~SIeventClassifications()
+			{
+				if (data) free(data);
+			}
+
+			void get(std::string &contentClassifications, std::string &userClassifications) const
+			{
+				contentClassifications.clear();
+				userClassifications.clear();
+				uint8_t *d = data;
+				unsigned int z = size & ~1;
+				for (unsigned int i = 0; i < z; i += 2) {
+					contentClassifications.append(1, (char)(*d++));
+					userClassifications.append(1, (char)(*d++));
+				}
+			}
+
+			ssize_t reserve(unsigned int r)
+			{
+				size_t off = size;
+
+				if (off) {
+					uint8_t * _data = (uint8_t *) realloc(data, size + r);
+					if (!_data)
+						return -1;
+					data = _data;
+				} else
+					data = (uint8_t *) malloc(r);
+				size += r;
+				return off;
+			}
+
+			ssize_t set(ssize_t off, uint8_t content, uint8_t user)
+			{
+				if (off < -1 || off + 2 >= (ssize_t) size)
+					return -1;
+				data[off++] = content;
+				data[off++] = user;
+				return off;
+			}
+
+			ssize_t set(ssize_t off, const uint8_t *_data, size_t _size)
+			{
+				if (off < -1 || off + _size >= size)
+					return -1;
+				memcpy (data + off, _data, _size);
+				size += _size;
+				return size;
+			}
+		};
+
+		SIeventClassifications classifications;
 
 		SIevent(const t_original_network_id, const t_transport_stream_id, const t_service_id, const unsigned short);
 		SIevent(void) {
@@ -400,15 +500,23 @@ class SIevent
 		// Name aus dem Short-Event-Descriptor
 		std::string getName() const;
 		void setName(const std::string &lang, const std::string &name);
+		void setName(unsigned int lang, const std::string &name);
 
 		// Text aus dem Short-Event-Descriptor
 		std::string getText() const;
 		void setText(const std::string &lang, const std::string &text);
+		void setText(unsigned int lang, const std::string &text);
 
 		// Aus dem Extended Descriptor
 		std::string getExtendedText() const;
-		void appendExtendedText(const std::string &lang, const std::string &text);
-		void setExtendedText(const std::string &lang, const std::string &text);
+		void appendExtendedText(const std::string &lang, const std::string &text, bool append = true);
+		void appendExtendedText(unsigned int lang, const std::string &text, bool append = true);
+		void setExtendedText(const std::string &lang, const std::string &text) {
+			appendExtendedText(lang, text, false);
+		}
+		void setExtendedText(unsigned int lang, const std::string &text) {
+			appendExtendedText(lang, text, false);
+		}
 
 		t_channel_id get_channel_id(void) const {
 			return CREATE_CHANNEL_ID(service_id, original_network_id, transport_stream_id);

src/eitd/SIlanguage.cpp
@@ -32,12 +32,35 @@
 #include <pthread.h>
 #include <fstream>
 
+#include <OpenThreads/Thread>
+#include <OpenThreads/Condition>
+#include <OpenThreads/ScopedLock>
+
 #include <sectionsdclient/sectionsdclient.h>
 
 #include "SIlanguage.hpp"
 
-std::vector<std::string> SIlanguage::languages;
-pthread_mutex_t SIlanguage::languages_lock = PTHREAD_MUTEX_INITIALIZER;
+static OpenThreads::Mutex languages_lock;
+static std::vector<std::string> languages;
+
+static OpenThreads::Mutex langIndexMutex;
+std::vector<std::string> langIndex;
+
+static const std::string languageOFF = "OFF";
+
+unsigned int getLangIndex(const std::string &lang)
+{
+	unsigned int ix = 0;
+	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(langIndexMutex);
+	if (!langIndex.size())
+		langIndex.push_back(languageOFF);
+	for (std::vector<std::string>::iterator it = langIndex.begin(); it != langIndex.end(); ++it, ++ix)
+		if (*it == lang)
+			return ix;
+	langIndex.push_back(lang);
+	return ix;
+}
+
 
 /*
 ALL = Show all available languages
@@ -49,22 +72,26 @@ ALL_ALL = Show all wanted languages if possible. If not found show all available
 //CSectionsdClient::SIlanguageMode_t SIlanguage::mode = CSectionsdClient::ALL;
 CSectionsdClient::SIlanguageMode_t SIlanguage::mode = CSectionsdClient::FIRST_ALL;
 
-void SIlanguage::filter(const std::map<std::string, std::string>& s, int max, std::string& retval)
+void SIlanguage::filter(const std::list<SILangData>& s, SILangData::SILangDataIndex textIndex, int max, std::string& retval)
 {
-	pthread_mutex_lock(&languages_lock);
 	// languages cannot get iterated through
 	// if another thread is updating it simultaneously
 	int count = max;
 
+	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(languages_lock);
 	if (mode != CSectionsdClient::ALL) {
 		for (std::vector<std::string>::const_iterator it = languages.begin() ;
 				it != languages.end() ; ++it) {
-			std::map<std::string,std::string>::const_iterator text;
-			if ((text = s.find(*it)) != s.end()) {
+			std::list<SILangData>::const_iterator text;
+			unsigned int lang = getLangIndex(*it);
+			for (text = s.begin(); text != s.end() && text->lang != lang; ++text);
+			if (text != s.end()) {
+				if (text->text[textIndex].empty())
+					continue;
 				if (count != max) {
 					retval.append(" \n");
 				}
-				retval.append(text->second);
+				retval.append(text->text[textIndex]);
 				if (--count == 0) break;
 				if (mode == CSectionsdClient::FIRST_FIRST ||
 						mode == CSectionsdClient::FIRST_ALL) {
@@ -77,12 +104,14 @@ void SIlanguage::filter(const std::map<std::string, std::string>& s, int max, st
 	if (retval.length() == 0) {
 		// return all available languages
 		if (s.begin() != s.end()) {
-			for (std::map<std::string, std::string>::const_iterator it = s.begin() ;
+			for (std::list<SILangData>::const_iterator it = s.begin() ;
 					it != s.end() ; ++it) {
+				if (it->text[textIndex].empty())
+					continue;
 				if (it != s.begin()) {
 					retval.append(" \n");
 				}
-				retval.append(it->second);
+				retval.append(it->text[textIndex]);
 				if (--max == 0) break;
 				if (mode == CSectionsdClient::FIRST_FIRST ||
 						mode == CSectionsdClient::ALL_FIRST) {
@@ -91,13 +120,12 @@ void SIlanguage::filter(const std::map<std::string, std::string>& s, int max, st
 			}
 		}
 	}
-	pthread_mutex_unlock(&languages_lock);
 }
 
 
 bool SIlanguage::loadLanguages()
 {
-	pthread_mutex_lock(&languages_lock);
+	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(languages_lock);
 	std::ifstream file(LANGUAGEFILE);
 	std::string word;
 	CSectionsdClient::SIlanguageMode_t tmpMode = CSectionsdClient::FIRST_ALL;
@@ -125,20 +153,18 @@ bool SIlanguage::loadLanguages()
 	if (tmpLang.empty()) tmpLang.push_back("OFF");
 	languages = tmpLang;
 	mode = tmpMode;
-	pthread_mutex_unlock(&languages_lock);
 	return true;
 
 error:
 	tmpLang.push_back("OFF");
 	languages = tmpLang;
 	mode = tmpMode;
-	pthread_mutex_unlock(&languages_lock);
 	return false;
 }
 
 bool SIlanguage::saveLanguages()
 {
-	pthread_mutex_lock(&languages_lock);
+	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(languages_lock);
 	std::ofstream file(LANGUAGEFILE);
 	switch(mode) {
 	case CSectionsdClient::ALL:
@@ -171,26 +197,22 @@ bool SIlanguage::saveLanguages()
 	file.close();
 	if (file.fail()) goto error;
 
-	pthread_mutex_unlock(&languages_lock);
 	return true;
 
 error:
-	pthread_mutex_unlock(&languages_lock);
 	return false;
 }
 
 void SIlanguage::setLanguages(const std::vector<std::string>& newLanguages)
 {
-	pthread_mutex_lock(&languages_lock);
+	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(languages_lock);
 	languages = newLanguages;
-	pthread_mutex_unlock(&languages_lock);
 }
 
 std::vector<std::string> SIlanguage::getLanguages()
 {
-	pthread_mutex_lock(&languages_lock);
+	OpenThreads::ScopedLock<OpenThreads::Mutex> m_lock(languages_lock);
 	std::vector<std::string> retval(languages);  // Store it before unlock
-	pthread_mutex_unlock(&languages_lock);
 	return retval;
 }
 

src/eitd/SIlanguage.hpp
@@ -25,15 +25,22 @@
 
 #include <string>
 #include <vector>
-#include <map>
+#include <list>
 
 #include <sectionsdclient/sectionsdclient.h>
 
 #define LANGUAGEFILE CONFIGDIR "/epglanguages.conf"
 
+class SILangData {
+public:
+	enum SILangDataIndex { langName = 0, langText, langExtendedText, langMax };
+	unsigned int lang;
+	std::string text[langMax];
+};
+
 class SIlanguage {
 public:
-	static void filter(const std::map<std::string, std::string>& s, int max, std::string& retval);
+	static void filter(const std::list<SILangData>& s, SILangData::SILangDataIndex textIndex, int max, std::string& retval);
 	static bool loadLanguages();
 	static bool saveLanguages();
 	static void setLanguages(const std::vector<std::string>& newLanguages);
@@ -44,9 +51,9 @@ public:
 private:
 	SIlanguage();
 	~SIlanguage();
-	static std::vector<std::string> languages;
-	static pthread_mutex_t languages_lock;
 	static CSectionsdClient::SIlanguageMode_t mode;
 };
 
+extern std::vector<std::string> langIndex;
+unsigned int getLangIndex(const std::string &lang);
 #endif

src/eitd/sectionsd.cpp
@@ -371,8 +371,7 @@ xprintf("addEvent: ch %012" PRIx64 " running %d (%s) got_CN %d\n", evt.get_chann
 		already_exists = false;
 
 	if ((already_exists) && (SIlanguage::getMode() == CSectionsdClient::LANGUAGE_MODE_OFF)) {
-		si->second->contentClassification = evt.contentClassification;
-		si->second->userClassification = evt.userClassification;
+		si->second->classifications = evt.classifications;
 #ifdef USE_ITEM_DESCRIPTION
 		si->second->itemDescription = evt.itemDescription;
 		si->second->item = evt.item;
@@ -380,11 +379,11 @@ xprintf("addEvent: ch %012" PRIx64 " running %d (%s) got_CN %d\n", evt.get_chann
 		//si->second->vps = evt.vps;
 		if ((evt.getExtendedText().length() > 0) && !evt.times.empty() &&
 				(evt.times.begin()->startzeit < zeit + secondsExtendedTextCache))
-			si->second->setExtendedText("OFF",evt.getExtendedText().c_str());
+			si->second->setExtendedText(0 /*"OFF"*/,evt.getExtendedText());
 		if (evt.getText().length() > 0)
-			si->second->setText("OFF",evt.getText().c_str());
+			si->second->setText(0 /*"OFF"*/,evt.getText());
 		if (evt.getName().length() > 0)
-			si->second->setName("OFF",evt.getName().c_str());
+			si->second->setName(0 /*"OFF"*/,evt.getName());
 	}
 	else {
 
@@ -402,7 +401,7 @@ xprintf("addEvent: ch %012" PRIx64 " running %d (%s) got_CN %d\n", evt.get_chann
 		//Strip ExtendedDescription if too far in the future
 		if ((e->times.begin()->startzeit > zeit + secondsExtendedTextCache) &&
 				(SIlanguage::getMode() == CSectionsdClient::LANGUAGE_MODE_OFF) && (zeit != 0))
-			e->setExtendedText("OFF","");
+			e->setExtendedText(0 /*"OFF"*/,"");
 
 		/*
 		 * this is test code, so indentation is deliberately wrong :-)
@@ -2651,8 +2650,7 @@ bool CEitManager::getEPGid(const event_id_t epgID, const time_t startzeit, CEPGD
 			epgdata->info1 = evt.getText();
 			epgdata->info2 = evt.getExtendedText();
 			/* FIXME printf("itemDescription: %s\n", evt.itemDescription.c_str()); */
-			epgdata->contentClassification = std::string(evt.contentClassification.data(), evt.contentClassification.length());
-			epgdata->userClassification = std::string(evt.userClassification.data(), evt.userClassification.length());
+			evt.classifications.get(epgdata->contentClassification, epgdata->userClassification);
 			epgdata->fsk = evt.getFSK();
 			epgdata->table_id = evt.table_id;
 
@@ -2713,8 +2711,7 @@ bool CEitManager::getActualEPGServiceKey(const t_channel_id channel_id, CEPGData
 		epgdata->info1 = evt.getText();
 		epgdata->info2 = evt.getExtendedText();
 		/* FIXME printf("itemDescription: %s\n", evt.itemDescription.c_str());*/
-		epgdata->contentClassification = std::string(evt.contentClassification.data(), evt.contentClassification.length());
-		epgdata->userClassification = std::string(evt.userClassification.data(), evt.userClassification.length());
+		evt.classifications.get(epgdata->contentClassification, epgdata->userClassification);
 		epgdata->fsk = evt.getFSK();
 		epgdata->table_id = evt.table_id;
 
@@ -2814,7 +2811,7 @@ bool CEitManager::getComponentTagsUniqueKey(const event_id_t uniqueKey, CSection
 		ret = true;
 
 		for (SIcomponents::iterator cmp = eFirst->second->components.begin(); cmp != eFirst->second->components.end(); ++cmp) {
-			response.component = cmp->component;
+			response.component = cmp->getComponentName();
 			response.componentType = cmp->componentType;
 			response.componentTag = cmp->componentTag;
 			response.streamContent = cmp->streamContent;

src/eitd/xmlutil.cpp
@@ -319,6 +319,7 @@ void *insertEventsfromFile(void * data)
 			while (event) {
 				SIevent e(onid,tsid,sid,xmlGetNumericAttribute(event, "id", 16));
 				uint8_t tid = xmlGetNumericAttribute(event, "tid", 16);
+				std::string contentClassification, userClassification;
 				if(tid)
 					e.table_id = tid;
 				e.table_id |= 0x80; /* make sure on-air data has a lower table_id */
@@ -376,9 +377,9 @@ void *insertEventsfromFile(void * data)
 				node = event->xmlChildrenNode;
 				while ((node = xmlGetNextOccurence(node, "content"))) {
 					char cl = xmlGetNumericAttribute(node, "class", 16);
-					e.contentClassification += cl;
+					contentClassification += cl;
 					cl = xmlGetNumericAttribute(node, "user", 16);
-					e.userClassification += cl;
+					userClassification += cl;
 					node = node->xmlNextNode;
 				}
 
@@ -390,7 +391,7 @@ void *insertEventsfromFile(void * data)
 					c.componentTag = xmlGetNumericAttribute(node, "tag", 16);
 					char *s = xmlGetAttribute(node, "text");
 					if (s)
-						c.component = s;
+						c.setComponent(s);
 					//e.components.insert(c);
 					e.components.push_back(c);
 					node = node->xmlNextNode;
@@ -423,6 +424,12 @@ void *insertEventsfromFile(void * data)
 					node = node->xmlNextNode;
 				}
 
+				if (contentClassification.size()) {
+					ssize_t off = e.classifications.reserve(2 * contentClassification.size());
+					if (off > -1)
+						for (unsigned i = 0; i < contentClassification.size(); i++)
+							off = e.classifications.set(off, contentClassification.at(i), userClassification.at(i));
+				}
 				addEvent(e, 0);
 				ev_count++;
 

commit 768d964b6c05f54a8f8a50a6ae486c8d7f3e9ce4

eitd: fix content classification bug introduced in 6eb8fa1c9e3a70a1e60465b838fe954c51e478b6

src/eitd/SIevents.cpp
@@ -234,11 +234,13 @@ void SIevent::parse(Event &event)
 		{
 			const ContentDescriptor * d = (ContentDescriptor *) *dit;
 			const ContentClassificationList *clist = d->getClassifications();
-			ssize_t off = classifications.reserve(clist->size() * 2);
-			for (ContentClassificationConstIterator cit = clist->begin(); cit != clist->end(); ++cit)
-				off = classifications.set(off,
-							  (*cit)->getContentNibbleLevel1() << 4 | (*cit)->getContentNibbleLevel2(),
-							  (*cit)->getUserNibble1() << 4 | (*cit)->getUserNibble2());
+			if (clist->size()) {
+				ssize_t off = classifications.reserve(clist->size() * 2);
+				for (ContentClassificationConstIterator cit = clist->begin(); cit != clist->end(); ++cit)
+					off = classifications.set(off,
+								  (*cit)->getContentNibbleLevel1() << 4 | (*cit)->getContentNibbleLevel2(),
+								  (*cit)->getUserNibble1() << 4 | (*cit)->getUserNibble2());
+			}
 			break;
 		}
 		case COMPONENT_DESCRIPTOR:
@@ -301,21 +303,21 @@ void SIevent::parseDescriptors(const uint8_t *des, unsigned len)
 	while(len>=sizeof(struct descr_generic_header)) {
 		desc=(struct descr_generic_header *)des;
 		/*printf("Type: %s\n", decode_descr(desc->descriptor_tag)); */
-		if(desc->descriptor_tag==0x4D)
+		if(desc->descriptor_tag==SHORT_EVENT_DESCRIPTOR)
 			parseShortEventDescriptor((const uint8_t *)desc, len);
-		else if(desc->descriptor_tag==0x4E)
+		else if(desc->descriptor_tag==EXTENDED_EVENT_DESCRIPTOR)
 			parseExtendedEventDescriptor((const uint8_t *)desc, len);
-		else if(desc->descriptor_tag==0x54)
+		else if(desc->descriptor_tag==CONTENT_DESCRIPTOR)
 			parseContentDescriptor((const uint8_t *)desc, len);
-		else if(desc->descriptor_tag==0x50)
+		else if(desc->descriptor_tag==COMPONENT_DESCRIPTOR)
 			parseComponentDescriptor((const uint8_t *)desc, len);
-		else if(desc->descriptor_tag==0x55)
+		else if(desc->descriptor_tag==PARENTAL_RATING_DESCRIPTOR)
 			parseParentalRatingDescriptor((const uint8_t *)desc, len);
-		else if(desc->descriptor_tag==0x4A) {
+		else if(desc->descriptor_tag==LINKAGE_DESCRIPTOR) {
 			parseLinkageDescriptor((const uint8_t *)desc, len);
 		}
 #if 0
-		else if(desc->descriptor_tag==0x69)
+		else if(desc->descriptor_tag==PDC_DESCRIPTOR)
 			parsePDCDescriptor((const char *)desc, e, len);
 #endif
 		if((unsigned)(desc->descriptor_length+2)>len)
@@ -396,6 +398,8 @@ void SIevent::parseContentDescriptor(const uint8_t *buf, unsigned maxlen)
 {
 	struct descr_generic_header *cont=(struct descr_generic_header *)buf;
 	if(cont->descriptor_length+sizeof(struct descr_generic_header)>maxlen)
+		return;
+	if(!cont->descriptor_length)
 		return;
 	ssize_t off = classifications.reserve(cont->descriptor_length);
 	classifications.set(off, buf + sizeof(struct descr_generic_header), cont->descriptor_length);

src/eitd/SIevents.hpp
@@ -382,14 +382,18 @@ class SIevent
 			SIeventClassifications& operator = (const SIeventClassifications& c)
 			{
 				if (this != &c) {
-					if (data)
+					size = 0;
+					if (data) {
 						free(data);
+						data = NULL;
+					}
 					if (c.data) {
 						data = (uint8_t *) malloc(c.size);
-						memcpy(data, c.data, c.size);
-					} else
-						data = NULL;
-					size = c.size;
+						if (data) {
+							memcpy(data, c.data, c.size);
+							size = c.size;
+						}
+					}
 				}
 				return *this;
 			}
@@ -397,12 +401,8 @@ class SIevent
 			SIeventClassifications(const SIeventClassifications& c)
 			{
 				if (this != &c) {
-					if (c.data) {
-						data = (uint8_t *) malloc(c.size);
-						memcpy(data, c.data, c.size);
-					} else
-						data = NULL;
-					size = c.size;
+					data = NULL;
+					*this = c;
 				}
 			}
 
@@ -419,7 +419,7 @@ class SIevent
 
 			bool operator!=(const SIeventClassifications& c) const
 			{
-				return *this == c;
+				return *this != c;
 			}
 
 			SIeventClassifications()
@@ -430,52 +430,61 @@ class SIevent
 
 			~SIeventClassifications()
 			{
-				if (data) free(data);
+				if (data)
+					free(data);
 			}
 
 			void get(std::string &contentClassifications, std::string &userClassifications) const
 			{
 				contentClassifications.clear();
 				userClassifications.clear();
-				uint8_t *d = data;
-				unsigned int z = size & ~1;
-				for (unsigned int i = 0; i < z; i += 2) {
-					contentClassifications.append(1, (char)(*d++));
-					userClassifications.append(1, (char)(*d++));
+				if (size) {
+					uint8_t *d = data, *e = data + size;
+					uint8_t cc[size/2], uc[size/2];
+					for (unsigned int i = 0; d < e; i++) {
+						cc[i] = *d++;
+						uc[i] = *d++;
+					}
+					contentClassifications.assign((char *) cc, size/2);
+					userClassifications.assign((char *) uc, size/2);
 				}
 			}
 
 			ssize_t reserve(unsigned int r)
 			{
-				size_t off = size;
+				if (r & 1)
+					return -1;
 
-				if (off) {
+				if (size) {
 					uint8_t * _data = (uint8_t *) realloc(data, size + r);
 					if (!_data)
 						return -1;
 					data = _data;
-				} else
+				} else {
 					data = (uint8_t *) malloc(r);
+					if (!data)
+						return -1;
+				}
+				size_t off = size;
 				size += r;
 				return off;
 			}
 
 			ssize_t set(ssize_t off, uint8_t content, uint8_t user)
 			{
-				if (off < -1 || off + 2 >= (ssize_t) size)
+				if (off < 0 || off + 2 > (ssize_t) size)
 					return -1;
 				data[off++] = content;
 				data[off++] = user;
 				return off;
 			}
 
-			ssize_t set(ssize_t off, const uint8_t *_data, size_t _size)
+			ssize_t set(ssize_t off, const uint8_t *_data, size_t len)
 			{
-				if (off < -1 || off + _size >= size)
+				if (len & 1 || off < 0 || off + len > size)
 					return -1;
-				memcpy (data + off, _data, _size);
-				size += _size;
-				return size;
+				memcpy (data + off, _data, len);
+				return off + len;
 			}
 		};
 